394bfdd94e49deccf4cfbae4472fb36abedb7812
ashishpandeyjava <ashishpandey.java@gmail.com>
2017-03-22 14:56:45 +0530

    LatLong file

---
+++ src/main/java/com/example/shop/LatLong.java
@@ -0,0 +1,35 @@
+package com.example.shop;
+
+public class LatLong {
+	
+	public double lat;
+	public double lng;
+	
+	
+	public LatLong() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public LatLong(double lat, double lng) {
+		super();
+		this.lat = lat;
+		this.lng = lng;
+	}
+	public double getLat() {
+		return lat;
+	}
+	public void setLat(double lat) {
+		this.lat = lat;
+	}
+	public double getLng() {
+		return lng;
+	}
+	public void setLng(double lng) {
+		this.lng = lng;
+	}
+	
+	
+	
+	
+
+}
